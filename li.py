df6d24e6b8abdeeecdc76153063708348f58a9bf
Hachibkhan <hachib.khan19@gmail.com>
2019-05-06 16:11:15 +0600

    Some function example

---
+++ li.py
@@ -0,0 +1,15 @@
+def add_numbers(numbers):
+
+    result = 0
+
+    for number in numbers:
+
+        result = result + number
+
+        return result
+
+result = add_numbers([1, 2, 30, 4, 5, 9])
+
+print(result)
+
+
---
+++ li_examp.py
@@ -0,0 +1,13 @@
+def test_fnc(li):
+
+    li[0] = 10
+
+my_li = [1, 2, 3, 4]
+
+print("before function call:", my_li)
+
+test_fnc(my_li)
+
+print("After function call:", my_li)
+
+
